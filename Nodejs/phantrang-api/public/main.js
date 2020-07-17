57c98325157b12b96cf7fd665a1fe12ec153d870
hoaivan0101 <hoaivan0101@yahoo.com>
2020-07-17 22:58:24 +0700

    check

--- Nodejs/phantrang-api/public/main.js
+++ Nodejs/phantrang-api/public/main.js
@@ -4,11 +4,17 @@ $.ajax({
     type:'GET'
 })
     .then(data => {
-        $('#content').html('')
+      $('#cont').html('')
         for (let i=0;i<data.length;i++){
-        $('#content').append($(`
-            <h3>${data[i].name}</h3>
-            <h3>${data[i].age}</h3>
+        $('#cont').append($(`
+        <div class="card d-inline-flex m-3" style="width: 12rem;">
+        <img src="./public/butterfly-grass-on-meadow-night-260nw-1111729556.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"">${data[i].name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Add to cart</a>
+        </div>
+        </div>
         `))
     }
     })
@@ -16,3 +22,4 @@ $.ajax({
         console.log(err);
     })
 }
+
