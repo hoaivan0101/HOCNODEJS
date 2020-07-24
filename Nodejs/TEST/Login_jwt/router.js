a78514e6045f20c8b1909017f0056c7a48bf8d07
hoaivan0101 <hoaivan0101@yahoo.com>
2020-07-24 23:38:04 +0700

    jwt

--- Nodejs/TEST/Login_jwt/router.js
+++ Nodejs/TEST/Login_jwt/router.js
@@ -1,30 +1,35 @@
-const express=require('express')
+const express = require('express')
 var router = express.Router()
 var data = require('./data')
 var jwt = require('jsonwebtoken');
 
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     res.sendfile('index.html')
 })
 
-router.post('/',function(req,res){
+router.post('/', function (req, res) {
     data.findOne(req.body)
-    .then(data=>{
-        if (data){
-        res.json({status:'SUCCES',
-        token:jwt.sign(data.name,'123')})}
-        else{res.json('KHONG TIM THAY ACCOUNT')}
-    })
-    .catch(err=>{res.json(err)})
+        .then(data => {
+            if (data) {
+                res.json({
+                    status: 'SUCCES',
+                    token: jwt.sign(data.name, '123')
+                })
+            }
+            else { res.json('KHONG TIM THAY ACCOUNT') }
+        })
+        .catch(err => { res.json(err) })
 })
 
-router.get('/home',function(req,res,next){
-    try {var ketqua = jwt.verify(req.cookies.token,'123')
+router.get('/home', function (req, res, next) {
+    try {
+        var ketqua = jwt.verify(req.cookies.token, '123')
         next()
     } catch (error) {
         res.redirect('/user')
     }
-},function(req,res,next){
+}, function (req, res, next) {
     res.json('WELCOME USERS HOME')
 })
-module.exports =router;
+
+module.exports = router;
