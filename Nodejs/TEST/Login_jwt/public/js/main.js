0fd7a8db254378fca74b343758152999d5cd43ea
hoaivan0101 <hoaivan0101@yahoo.com>
2020-07-27 10:36:27 +0700

    loginjwt

--- Nodejs/TEST/Login_jwt/public/js/main.js
+++ Nodejs/TEST/Login_jwt/public/js/main.js
@@ -5,22 +5,6 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for (var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-
 $('.btn').click(function () {
     $.ajax({
         url: '/user',
